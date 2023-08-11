eee3963a959082ad9a78c0af41b5c9de65527642
noob616161 <124112789+noob616161@users.noreply.github.com>
2023-08-11 10:27:56 +0800

    Create 老.白名单.lua

---
+++ qtjb/__________China666___________China First_______________China YYDS/老.白名单.lua
@@ -0,0 +1,52 @@
+local a=tostring(game.Players.LocalPlayer.Character);
+if a=="123fa98"then
+_G.bmd=true
+elseif a=="游戏名"then
+_G.bmd=true
+elseif a=="游戏名"then
+_G.bmd=true
+elseif a=="游戏名"then
+_G.bmd=true
+elseif a=="游戏名"then
+_G.bmd=true
+elseif a=="游戏名"then
+_G.bmd=true
+elseif a=="游戏名"then
+_G.bmd=true
+elseif a=="游戏名"then
+_G.bmd=true
+elseif a=="游戏名"then
+_G.bmd=true
+elseif a=="游戏名"then
+_G.bmd=true
+elseif a=="游戏名"then
+_G.bmd=true
+elseif a=="游戏名"then
+_G.bmd=true
+elseif a=="游戏名"then
+_G.bmd=true
+elseif a=="游戏名"then
+_G.bmd=true
+elseif a=="游戏名"then
+_G.bmd=true
+elseif a=="游戏名"then
+_G.bmd=true
+elseif a=="游戏名"then
+_G.bmd=true
+elseif a=="游戏名"then
+_G.bmd=true
+elseif a=="游戏名"then
+_G.bmd=true
+end
+if _G.bmd==true then
+local CoreGui = game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification", {
+    Title = "老 脚 本",
+    Text = a.."欢迎您",
+    Duration = 5,
+})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/noob616161/KphoooOooooOoOo-wprfbdksorbfKvsdcIUH-194jddJ-___-_-lIlIIIllIIIlllIIIIlIlIIlIllIIIllllllIllllllIIIl/main/qtjb/__________China666___________China%20First_______________China%20YYDS/%E8%80%81.lua"))();
+      else
+  setclipboard("681256318")
+  game.Players.LocalPlayer:Kick("请加681256318获得白名单")
+end
